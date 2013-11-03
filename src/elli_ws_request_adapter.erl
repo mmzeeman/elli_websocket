6d3c619839e225f5197f0e3b2044a5a79d2f1d74
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-11-03 15:27:15 +0100

    Added explanations

--- src/elli_ws_request_adapter.erl
+++ src/elli_ws_request_adapter.erl
@@ -1,6 +1,21 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2013 Maas-Maarten Zeeman
 %%
-%% Module with functions to help use easily use the cowboy ws handler inside elli.
+%% @doc Elli WebSocket Request Adapter.
 %%
+%% Copyright 2013 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(elli_ws_request_adapter).
 
@@ -24,11 +39,10 @@
 	websocket_handler_terminate/4
 	]).
 
-%% 
+%% Helper function.
 -export([messages/1]).
 
-
-%% 
+%% Request adapter record.
 -record(req_adapter, {
 	req,
 
@@ -56,9 +70,8 @@ init(#req{}=Req, RespCompress) ->
 	#req_adapter{req=Req, resp_compress=RespCompress}.
 
 
-%%
-%% mimics cowboy_req:get/2
-%%
+% @doc Mimics cowboy_req:get/2
+%
 get(socket, ReqAdapter) ->
 	Req = ReqAdapter#req_adapter.req, 
 	Req#req.socket;
@@ -66,12 +79,14 @@ get(resp_compress, ReqAdapter) ->
 	ReqAdapter#req_adapter.resp_compress;
 get(L, Req) when is_list(L) ->
 	get(L, Req, []).
-
 get([], _Req, Acc) ->
  	lists:reverse(Acc);
 get([H|T], Req, Acc) ->
  	get(T, Req, [get(H, Req)|Acc]).
 
+
+% @doc Mimics cowboy_req:maybe_reply/2
+%
 maybe_reply(400, ReqAdapter) ->
 	case ReqAdapter#req_adapter.sent_upgrade_reply of
 		true ->
@@ -80,16 +95,12 @@ maybe_reply(400, ReqAdapter) ->
 			reply(400, ReqAdapter)
 	end.
 
+
+% @doc Mimics cowboy_req:ensure_response/2
+%
 ensure_repsonse(ReqAdapter, 400) ->
 	reply(400, ReqAdapter).
 
-reply(400, #req_adapter{req=Req}) ->
-	Body = <<"Bad request">>,
-    Size = size(Body),
-
-    %% We should signal that we want to close the connection too.
-    elli_http:send_response(Req, 400, [{"Connection", "close"},
-                                       {"Content-Length", Size}], Body).
 
 %% 
 %% Send an upgrade reply to the 
@@ -103,8 +114,8 @@ upgrade_reply(101, Headers, #req_adapter{req=Req}=RA) ->
 %% Note: The headers keys are already parsed by Erlang decode_packet. This
 %% means that all keys are capitalized.
 
-%% @doc {ok, ParsedHeaders, Req}
-
+% @doc Mimics cowboy_req:parse_header/3 {ok, ParsedHeaders, Req}
+%
 parse_header(<<"upgrade">>, #req_adapter{req=Req}=RA) ->
 	%% case insensitive tokens.
 	Values = get_header_values(<<"Upgrade">>, Req),
@@ -120,15 +131,16 @@ parse_header(<<"sec-websocket-extensions">>, #req_adapter{req=Req}=RA) ->
 	Extensions = [{E, []} || E <- Exts, E =:= <<"x-webkit-deflate-frame">>],
 	{ok, Extensions, RA}.
 
-%% @doc
+% @doc Mimics cowboy_req:header/2
+%
 header(<<"sec-websocket-version">>, #req_adapter{req=Req}=RA) ->
 	{get_header_value(<<"Sec-WebSocket-Version">>, Req), RA};
 header(<<"sec-websocket-key">>, #req_adapter{req=Req}=RA) ->
 	{get_header_value(<<"Sec-Websocket-Key">>, Req), RA}.
 
 
-%%
-%% 
+% @doc Mimics cowboy_req:set_meta/3
+% 
 set_meta(websocket_version, Version, ReqAdapter) ->
 	ReqAdapter#req_adapter{websocket_version = Version};
 set_meta(websocket_compress, Bool, ReqAdapter) ->
@@ -137,13 +149,18 @@ set_meta(websocket_compress, Bool, ReqAdapter) ->
 
 %% @doc Initialize the websocket handler.
 
-%% calls websocket_init(Req, HandlerOpts) ->
-%%     {ok, Headers, HandlerState}
-%%     {ok, Headers, hibernate, HandlerState}
-%%     {ok, Headers, Timeout, HandlerState}
-%%     {ok, Headers, hibernate, Timeout, HandlerState}
-%%     {shutdown, Headers}
-
+% @doc Call the websocket_init callback of the websocket handler.
+%
+% calls websocket_init(Req, HandlerOpts) ->
+%     {ok, Headers, HandlerState} - We can upgrade, headers are added to the upgrade response.
+%     {ok, Headers, hibernate, HandlerState} - We can upgrade, but this process will hibernate, headers 
+%         are added to the upgrade response
+%     {ok, Headers, Timeout, HandlerState} - We can upgrade, we will timout, headers are added to the 
+%         upgrade respose.
+%     {ok, Headers, hibernate, Timeout, HandlerState} - We can upgrade, set a timeout and hibernate. 
+%         Headers are added to the response.
+%     {shutdown, Headers} - We can't upgrade, a bad request response will be sent to the client.
+%
 websocket_handler_init(#req_adapter{req=Req}=RA, Handler, HandlerOpts) ->
 	case Handler:websocket_init(Req, HandlerOpts) of
 		{shutdown, Headers} ->
@@ -188,6 +205,15 @@ messages({ssl, _}) ->
 %%
 %% Helpers
 %%
+
+% @doc Send a bad_request reply.
+%
+reply(400, #req_adapter{req=Req}) ->
+    Body = <<"Bad request">>,
+    Size = size(Body),
+
+    elli_http:send_response(Req, 400, [{"Connection", "close"},
+                                       {"Content-Length", Size}], Body).
 
 %% Get all header values for Key
 get_header_values(Key, #req{headers=Headers}) ->
