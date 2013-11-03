b0aa6662318464e4411cce9f0c986abccc077a3b
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-11-03 15:14:07 +0100

    Added license

--- src/elli_bstr.erl
+++ src/elli_bstr.erl
@@ -1,6 +1,21 @@
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2013 Maas-Maarten Zeeman
 %%
-%% Binary string helper
+%% @doc Binary String Helper Functions
+%%
+%% Copyright 2013 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
 %% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(elli_bstr).
 
@@ -77,6 +92,10 @@ case_insensitive_equal_test() ->
 
         ?assertEqual(true, is_equal_ci(<<"aBc">>, <<"abc">>)),
         ?assertEqual(true, is_equal_ci(<<"123AB">>, <<"123ab">>)),
+
+        ?assertEqual(false, is_equal_ci(<<"1">>, <<"123ab">>)),
+        ?assertEqual(false, is_equal_ci(<<"">>, <<"123ab">>)),
+        ?assertEqual(false, is_equal_ci(<<"">>, <<" ">>)),
 
         ok.
 
