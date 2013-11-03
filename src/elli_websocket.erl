baca2229c4c40a05ab0cb4fb04f7b09168f0d0fc
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-11-03 15:09:07 +0100

    Added documentation

--- src/elli_websocket.erl
+++ src/elli_websocket.erl
@@ -1,5 +1,21 @@
-%% @doc: Elli websocket handler.
+%% @author Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
+%% @copyright 2012, 2013 Maas-Maarten Zeeman
 %%
+%% @doc Elli Websocket Handler 
+%%
+%% Copyright 2013 Maas-Maarten Zeeman
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
 
 -module(elli_websocket).
 -author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
@@ -8,39 +24,21 @@
 
 -export([upgrade/2]).
 
-
-% @doc 
+% @doc Upgrade the request to a websocket request. 
+%    Args: proplist with settings.
+%            handler: Websocket callback module
+%            handler_opts: Options to pass to the websocket_init 
+%                callback.
+%            resp_compress: bool(), when set to true the traffic 
+%                will be compressed if the client supports it.
 %
 upgrade(Req, Args) ->
-	RespCompress = proplists:get_value(resp_compress, Args, false),
-	ReqAdapter = elli_ws_request_adapter:init(Req, RespCompress),
-	{handler, Handler} = proplists:lookup(handler, Args),
-	HandlerOpts = proplists:get_value(handler_opts, Args, []),
+    RespCompress = proplists:get_value(resp_compress, Args, false),
+    ReqAdapter = elli_ws_request_adapter:init(Req, RespCompress),
+    {handler, Handler} = proplists:lookup(handler, Args),
+    HandlerOpts = proplists:get_value(handler_opts, Args, []),
 
-	%% Adapter is ready, hand over to ws_protocol
-	elli_ws_protocol:upgrade(ReqAdapter, Args, Handler, HandlerOpts),
+    %% Adapter is ready, hand over to ws_protocol
+    elli_ws_protocol:upgrade(ReqAdapter, Args, Handler, HandlerOpts),
     ok.
 
-
-%%
-%% TESTS
-%%
-
--ifdef(TEST).
-
--include_lib("eunit/include/eunit.hrl").
-
-config() ->
-    [
-        {callback, elli_middleware},
-        {callback_args,
-        [{mods, [
-            {elli_ws_example, []},
-            {elli_example_callback, []}
-        ]}]}
-    ].
-
-elli_ws_test() ->
-	ok.
-
--endif.
